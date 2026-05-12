03e934e5a89891650cc245ebf881add710469511
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-05-12 21:04:27 +0600

    Create delete method, delete confirm dialog and delete action user

--- src/app/components/UsersTable.jsx
+++ src/app/components/UsersTable.jsx
@@ -1,7 +1,13 @@
-import { Button, Table } from "@heroui/react";
+'use client'
+import { AlertDialog, Button, Table } from "@heroui/react";
 import Link from "next/link";
+import { deleteUser } from "../lib/actions";
 
 const UsersTable = ({ users }) => {
+
+  const handleDelete =async (userId) => {
+    await deleteUser(userId)
+  }
   return (
     <Table>
       <Table.ScrollContainer>
@@ -25,9 +31,38 @@ const UsersTable = ({ users }) => {
                   <Link href={`/users/${user._id}`}>
                     <Button variant="outline">Edit</Button>
                   </Link>
-                  <Link href={`/users/${user._id}`}>
-                    <Button variant="danger-soft">Delete</Button>
-                  </Link>
+
+                  <AlertDialog>
+                    <Button onClick={()=> handleDelete(user._id)} variant="danger">Delete</Button>
+                    <AlertDialog.Backdrop>
+                      <AlertDialog.Container>
+                        <AlertDialog.Dialog className="sm:max-w-100">
+                          <AlertDialog.CloseTrigger />
+                          <AlertDialog.Header>
+                            <AlertDialog.Icon status="danger" />
+                            <AlertDialog.Heading>
+                              Delete project permanently?
+                            </AlertDialog.Heading>
+                          </AlertDialog.Header>
+                          <AlertDialog.Body>
+                            <p>
+                              This will permanently delete{" "}
+                              <strong>{user.name}</strong> and all of its data.
+                              This action cannot be undone.
+                            </p>
+                          </AlertDialog.Body>
+                          <AlertDialog.Footer>
+                            <Button slot="close" variant="tertiary">
+                              Cancel
+                            </Button>
+                            <Button slot="close" variant="danger">
+                              Delete Project
+                            </Button>
+                          </AlertDialog.Footer>
+                        </AlertDialog.Dialog>
+                      </AlertDialog.Container>
+                    </AlertDialog.Backdrop>
+                  </AlertDialog>
                 </Table.Cell>
               </Table.Row>
             ))}
